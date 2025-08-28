b8100c0e35ccc7317f858e74bd174fc0c4f4d1ea
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2025-08-28 08:48:39 +0300

    #14 Update exception handler

--- src/main/java/com/hotelmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hotelmanager/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.hotelmanager.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,6 +21,18 @@ public class GlobalExceptionHandler {
         ExceptionDetails apiError = new ExceptionDetails();
         apiError.setMessage(ex.getMessage());
         return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDetails> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        ExceptionDetails response = new ExceptionDetails(HttpStatus.BAD_REQUEST);
+        response.setMessage("Validation error");
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> response.addFieldError(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     private ResponseEntity<?> buildResponseEntity(ExceptionDetails apiError) {
