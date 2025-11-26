f2207bd190719fd313c383c5a72cdff987b54e26
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2025-11-27 00:09:14 +0200

    chor add private constructor to RoomSpecifications

--- src/main/java/com/hotelmanager/specifications/RoomSpecifications.java
+++ src/main/java/com/hotelmanager/specifications/RoomSpecifications.java
@@ -2,11 +2,14 @@ package com.hotelmanager.specifications;
 
 import com.hotelmanager.model.entity.Room;
 import com.hotelmanager.model.enums.RoomStatus;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.UUID;
 
-public class RoomSpecifications {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RoomSpecifications {
 
     public static Specification<Room> byRoomType(UUID type) {
         return (root, query, cb) -> cb.equal(root.get("roomType").get("uuid"), type);
